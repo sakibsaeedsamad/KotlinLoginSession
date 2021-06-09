db32468f53f566ec0b3556c9964d43bf149d99df
sakibssamad <sssakib.info@gmail.com>
2021-06-09 11:27:39 +0600

    final commit

--- app/src/main/java/com/sssakib/kotlinloginsession/DemoActivity.kt
+++ app/src/main/java/com/sssakib/kotlinloginsession/DemoActivity.kt
@@ -11,78 +11,26 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_demo.*
 
-class DemoActivity : AppCompatActivity() {
-    var handler: Handler? = null
-    var r: Runnable? = null
-    val DISCONNECT_TIMEOUT: Long = 30000
+class DemoActivity : SubActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo)
 
-        val intentFilter = IntentFilter(Intent.ACTION_SCREEN_ON)
-        intentFilter.addAction(Intent.ACTION_SCREEN_OFF)
-        registerReceiver(object : BroadcastReceiver() {
-            override fun onReceive(context: Context?, intent: Intent) {
-                if (intent.action == Intent.ACTION_SCREEN_OFF) {
-                    // Log.d(ContentValues.TAG, Intent.ACTION_SCREEN_OFF)
-                    onPause()
-                } else if (intent.action == Intent.ACTION_SCREEN_ON) {
-                    Log.d(TAG, Intent.ACTION_SCREEN_ON)
-                }
-            }
-        }, intentFilter)
 
 
         btnGoToWelcome.setOnClickListener {
-
+            finish()
             val i = Intent(this, WelcomeActivity::class.java)
             startActivity(i)
-            onStop()
+
         }
-        handler = Handler()
-        r = Runnable { // TODO Auto-generated method stub
-            val i = Intent(this, MainActivity::class.java)
-
-            startActivity(i)
-        }
-        onResume()
     }
 
-    override fun onUserInteraction() {
-        // TODO Auto-generated method stub
-        super.onUserInteraction()
-        onResume()
-    }
-
-    public override fun onResume() {
-        super.onResume()
-        resetDisconnectTimer()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        stopDisconnectTimer()
-    }
-
-    override fun onPause() {
-
-        super.onPause()
-        resetDisconnectTimer()
-
-    }
-
-    fun resetDisconnectTimer() {
-        handler?.removeCallbacks(r!!)
-        handler?.postDelayed(r!!, DISCONNECT_TIMEOUT)
-    }
-
-    fun stopDisconnectTimer() {
-        handler?.removeCallbacks(r!!)
-    }
 
     override fun onBackPressed() {
 
-        onStop()
+        finish()
 
         val i = Intent(this, WelcomeActivity::class.java)
 
---
+++ app/src/main/java/com/sssakib/kotlinloginsession/SubActivity.kt
@@ -0,0 +1,60 @@
+package com.sssakib.kotlinloginsession
+
+import android.content.*
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+open class SubActivity : AppCompatActivity() {
+    var myRunnable: Runnable
+    private var myHandler = Handler()
+    val DISCONNECT_TIMEOUT: Long = 10000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intentFilter = IntentFilter(Intent.ACTION_SCREEN_OFF)
+        registerReceiver(object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent) {
+                if (intent.action == Intent.ACTION_SCREEN_OFF) {
+                    start()
+                }
+            }
+        }, intentFilter)
+    }
+
+    init {
+        myRunnable = Runnable {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
+    }
+
+
+    override fun onUserInteraction() {
+        super.onUserInteraction();
+        stop()
+        start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        start()
+    }
+
+    fun start() {
+        myHandler.postDelayed(myRunnable, DISCONNECT_TIMEOUT)
+    }
+
+    fun stop() {
+        myHandler.removeCallbacks(myRunnable)
+    }
+}
